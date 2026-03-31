61b150d07bc5d68653b99539d6cab24f8815bb5d
2445993 <2445993@cegeplimoilou.ca>
2026-03-30 20:39:15 -0400

    Création de la méthodes separerElementsJson.

--- APP4XavierRoy/src/app/CircuitBuilder.java
+++ APP4XavierRoy/src/app/CircuitBuilder.java
@@ -2,6 +2,8 @@ package app;
 
 import electronique.Composant;
 
+import java.util.ArrayList;
+
 public class CircuitBuilder {
 
     public CircuitBuilder(){}
@@ -13,5 +15,64 @@ public class CircuitBuilder {
     private <JsonNode> Composant lireComposant(JsonNode composant) {
 
       return null;
+    }
+
+    public static String[] separerElementsJson(String composants) {
+        if (composants != null) {
+        int nbParanthesesOuvertes;
+        int nbParanthesesFermees;
+        int nbCrochetsOuverts;
+        int nbCrochetsFermes;
+        int debut;
+        ArrayList<String> elements = new ArrayList<>();
+        String[] lignes = composants.split("\n");
+        String composant;
+
+        composant = "";
+        debut = 0;
+        nbParanthesesOuvertes = 0;
+        nbParanthesesFermees = 0;
+        nbCrochetsOuverts = 0;
+        nbCrochetsFermes = 0;
+        do {
+            if (lignes[debut].contains("[")) {
+                    nbCrochetsOuverts++;
+            }
+            debut++;
+        } while (lignes[debut].contains("["));
+        int i = debut;
+        do {
+            lignes[i] = lignes[i].trim();
+            if (lignes[debut].contains("[")) {
+                nbCrochetsOuverts++;
+            }
+            if (lignes[debut].contains("]")) {
+                nbCrochetsFermes++;
+            }
+            if (lignes[i].contains("{")) {
+                nbParanthesesOuvertes++;
+                if (lignes[i].length() != 1) {
+                    composant += lignes[i] + "\n";
+                    if (lignes[i].contains("}")) {
+                            nbParanthesesFermees++;
+                    }
+                }
+            } else if (lignes[i].contains("}")) {
+                nbParanthesesFermees++;
+            } else {
+                composant += lignes[i] + "\n";
+            }
+            if (nbParanthesesOuvertes == nbParanthesesFermees) {
+                elements.add(composant);
+                composant = "";
+                nbParanthesesOuvertes = 0;
+                nbParanthesesFermees = 0;
+            }
+            i++;
+        } while (nbCrochetsOuverts != nbCrochetsFermes);
+        return (String[]) elements.toArray();
+        } else {
+            throw new IllegalArgumentException("code JSON invalide");
+        }
     }
 }
